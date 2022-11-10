ceb0bd061e849db1d7fbcb6433605fa4a7c5d92c
felipe <riopipe8@gmail.com>
2022-11-10 00:07:45 -0300

    Library Working

--- script.js
+++ script.js
@@ -1,40 +1,61 @@
-let myLibrary = [
-    {
-        name: "Jooks",
-        author: "ghhs. L.",
-        pages: 112,
-        isRead: false
-    }
-];
 const bookSection = document.querySelector(".myBooks");
 const button = document.querySelector(".button");
+const inputTitle = document.getElementById("title");
+const inputAuthor = document.getElementById("author");
+const inputPages = document.getElementById("pages");
+const readCheck = document.getElementById("check");
 
-function Book() {
-  // the constructor...
+function Book(title, autor, pages, isRead) {
+    this.title = title;
+    this.author = autor;
+    this.pages = pages;
+    this.isRead = isRead;
 }
+let booksTotal = 0;
+
+
+let myLibrary = [];
+
+
 button.addEventListener("click", addBookToLibrary);
 
-function addBookToLibrary(event) {
-    const bookContainer = document.createElement("DIV");
-    const book = document.createElement("DIV");
-    const bookName = document.createElement("DIV");
-    const bookNametxt = document.createElement("P");
+function addBookToLibrary() {
+const inputTitleV = inputTitle.value;
+const inputAuthorV = inputAuthor.value;
+const inputPagesV = inputPages.value;
 
-    const bookInfo = document.createElement("DIV");
+    if (inputTitleV && inputAuthorV && inputPagesV) {
+        const newBook = new Book(inputTitleV,inputAuthorV, inputPagesV);
+        myLibrary.push(newBook);
 
-    bookInfo.innerHTML = `
-    <p>Name: ${myLibrary[0].name}</p>
-    <p>Author: ${myLibrary[0].author}</p>
-    <p>Pages: ${myLibrary[0].pages}</p>
-    `;
-    bookNametxt.textContent = myLibrary[0].name;
-    bookContainer.classList.add("book-container");
-    book.classList.add("book");
-    bookName.classList.add("book-name");
+        const bookContainer = document.createElement("DIV");
+        const book = document.createElement("DIV");
+        const bookName = document.createElement("DIV");
+        const bookNametxt = document.createElement("P");
     
-    bookName.appendChild(bookNametxt);
-    book.appendChild(bookName);
-    bookContainer.appendChild(book);
-    bookContainer.appendChild(bookInfo);
-    bookSection.appendChild(bookContainer);;
+        const bookInfo = document.createElement("DIV");
+    
+        bookInfo.innerHTML = `
+        <p>Name: ${myLibrary[0].title}</p>
+        <p>Author: ${myLibrary[0].author}</p>
+        <p>Pages: ${myLibrary[0].pages}</p>
+        `;
+        bookNametxt.textContent = myLibrary[0].title;
+        bookContainer.classList.add("book-container");
+        book.classList.add("book");
+        bookName.classList.add("book-name");
+        
+        bookName.appendChild(bookNametxt);
+        book.appendChild(bookName);
+        bookContainer.appendChild(book);
+        bookContainer.appendChild(bookInfo);
+        bookSection.appendChild(bookContainer);;
+
+        clearInputs();
+    }
+}
+function clearInputs () {
+    inputTitle.value = "";
+    inputAuthor.value = "";
+    inputPages.value = "";
 }
